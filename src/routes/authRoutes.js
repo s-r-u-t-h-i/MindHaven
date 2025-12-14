307630bfcf955e9d7b44d6d77ca03cbcd25c67b9
Sruthi <itzmessruthi@gmail.com>
2025-12-14 23:03:19 +0530

    Edited routes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import User from "../models/User.js";
+import User from "../routes/models/User.js";
 import jwt from "jsonwebtoken";
 
 const router = express.Router();
